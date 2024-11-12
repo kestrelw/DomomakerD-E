a0d11d7222a9564df0eec503dd18596d74e7debf
kestrelw <mail4melodie@gmail.com>
2024-11-12 13:50:30 -0500

    heroku test

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
@@ -11,7 +11,8 @@ const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_Port || 3000;
 
-const dbURI = process.env.MONGODB_URI || 'mongodb+srv://myw4072:1Y7Yat2kSgzuOm7F@cluster0.tbsz4.mongodb.net/DomoMaker?retryWrites=true&w=majority';
+const dbURI = process.env.MONGODB_URI || require('../.mongo-connection.js');
+
 mongoose.connect(dbURI).catch((err) => {
   if (err) {
     console.log('Could not connect to database');
@@ -25,8 +26,8 @@ app.use(helmet());
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
